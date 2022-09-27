91b83114a35a0d626607bc4bced8c808d94fc429
kubicahere <kubica.here@gmail.com>
2022-09-27 10:53:21 +0200

    Created DataContext & firstMigration. Working with MS SQL Server

---
+++ MatchDataManager.Api/Database/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using MatchDataManager.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MatchDataManager.Api.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ MatchDataManager.Api/Migrations/20220927085114_firstMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchDataManager.Api.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CoachName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
