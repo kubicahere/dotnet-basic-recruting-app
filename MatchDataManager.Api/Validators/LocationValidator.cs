d2c5e13737cbc3088215d9e3dc4447253ea563f8
kubicahere <kubica.here@gmail.com>
2022-09-27 20:33:44 +0200

    Added data validators

---
+++ MatchDataManager.Api/Validators/LocationValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MatchDataManager.Api.Database;
+using MatchDataManager.Api.Models;
+using MatchDataManager.Api.Repositories;
+
+namespace MatchDataManager.Api.Validators
+{
+    public class LocationValidator : AbstractValidator<Location>
+    {
+        private readonly IRepository<Location> _repository;
+        public LocationValidator(IRepository<Location> repository)
+        {
+            _repository = repository;
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!").MaximumLength(255);
+            RuleFor(x => x.City).NotEmpty().WithMessage("City is required!").MaximumLength(55);
+            RuleFor(x => x.Name).Must(UniqueName).WithMessage("This name already exist!");
+
+        }
+
+        private bool UniqueName(string name)
+        {
+            return _repository.GetDataByName(name).FirstOrDefault(x => x.Name == name) == null;
+        }
+    }
+}
---
+++ MatchDataManager.Api/Validators/TeamValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MatchDataManager.Api.Models;
+using MatchDataManager.Api.Repositories;
+
+namespace MatchDataManager.Api.Validators
+{
+    public class TeamValidator : AbstractValidator<Team>
+    {
+        private readonly IRepository<Team> _repository;
+        public TeamValidator(IRepository<Team> repository)
+        {
+            _repository = repository;
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!").MaximumLength(255);
+            RuleFor(x => x.CoachName).NotEmpty().WithMessage("City is required!").MaximumLength(55);
+            RuleFor(x => x.Name).Must(UniqueName).WithMessage("This name already exist!");
+
+        }
+
+        private bool UniqueName(string name)
+        {
+            return _repository.GetDataByName(name).FirstOrDefault(x => x.Name == name) == null;
+        }
+    }
+}
